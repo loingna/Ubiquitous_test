69aa79b503bc27cae94f5ea4f2b01c380b5cae91
loingna <91304917+loingna@users.noreply.github.com>
2021-12-10 20:43:08 +0800

    pay to  proxy

---
+++ Transaction/Transaction.go
@@ -0,0 +1,28 @@
+/*根据操作的不同支付不同的费用
+  要有支付下载、上传、查询、复制、删除等操作的处理方法
+  Pay_upload()、Pay_download()、Pay_delect()、Pay_copy()、Pay_select()
+*/
+package Transaction
+
+var Transaction struct{
+
+
+}
+
+
+func (this *Transaction) Pay_upload(){
+
+}
+
+func (this *Transaction) Pay_download(){
+
+}
+func (this *Transaction) Pay_delect(){
+
+}
+func (this *Transaction) Pay_copy(){
+
+}
+func (this *Transaction) Pay_select(){
+
+}
