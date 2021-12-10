531f79b264e316bacfd287e9d351514d02e826ae
loingna <91304917+loingna@users.noreply.github.com>
2021-12-10 21:08:17 +0800

    Save important data

---
+++ Administration_account/Rigster_account.go
@@ -0,0 +1,34 @@
+/*
+  应该有注册ID方法、保存注册成功的ID、获取ID
+  管理认证、设置信誉等信息
+  
+  调用连接服务器的模块、接收服务器数据等
+ */
+package Administration_account
+
+import(
+   //
+)
+
+var Rigster_account struct{
+
+}
+func (this *Rigster_account) Getnewacount(){
+  return &Rigster_account{}
+}
+
+func (this *Rigster_account) RigsterId(){
+  
+}
+func (this *Rigster_account) Get_Rigster() int{
+  
+}
+func (this *Rigster_account) Rigster_Auth(){
+  
+}
+func (this *Rigster_account) Set_reputation(){
+  
+}
+func (this *Rigster_account) Get_reputation(){
+  
+}
