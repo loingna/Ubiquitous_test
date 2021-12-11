bc9d54c0094495aebaa6097711f9fa5c097d9d97
loingna <91304917+loingna@users.noreply.github.com>
2021-12-11 11:48:56 +0800

    Handle data

---
+++ File_handle/combination_data.go
@@ -0,0 +1,29 @@
+/*
+  传入一个文件，对文件进行处理
+  切片---切丝--加密--纠删编码---增和纠删码份额---返回处理后的数据
+  需要使用纠删码，加密模块
+ */
+
+type Hnadle_data struct{
+
+}
+//获取一个文件
+func (data *Handle_data) Get_file(filename string){
+
+}
+//将文件进行切片
+func (data *Handle_data) Cut_slice(){
+
+}
+//将文件进行切丝
+func (data *Handle_data) Cut_ribben(){
+
+}
+//组合纠删码份额
+func (data *Handle_data) Combine_data(filename string){
+
+}
+//返回整合后的数据
+func (data *Handle_data) Get_data(){
+
+}
