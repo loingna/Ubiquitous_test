b70722e78c7bb7c88b9ceef6bc6f6fdc9ab7932b
loingna <91304917+loingna@users.noreply.github.com>
2021-12-16 18:14:40 +0800

    Conection to Client

---
+++ Connecte_clint_proxy/ConnectionClint.go
@@ -0,0 +1,22 @@
+/*
+ 做为服务器，使用UDP/TCP与客户端建立连接
+ 
+*/
+
+// 建立连接，还应该有发送数据和接收数据的方法
+
+
+type ConnectionClint struct{
+  
+}
+
+
+func (this *ConnectionClint) Connection(){
+
+}
+
+func (this *ConnectionClint) Sendto(){
+}
+
+func (this *ConnectionClint) Responsefron(){
+}
