73fd1fa7c8e30464be700049605b34c86d0cd119
loingna <91304917+loingna@users.noreply.github.com>
2021-12-16 18:10:53 +0800

    Connection to Node

---
+++ Connecte_clint_proxy/ConnectionNode.go
@@ -0,0 +1,22 @@
+/*
+ 做为服务器，使用UDP/TCP与存储节点建立连接
+ 
+*/
+
+// 建立连接，还应该有发送数据和接收数据的方法
+
+
+type ConnectionNode struct{
+  
+}
+
+
+func (this *ConnectionNode) Connection(){
+
+}
+
+func (this *ConnectionNode) Sendto(){
+}
+
+func (this *ConnectionNode) Responsefron(){
+}
